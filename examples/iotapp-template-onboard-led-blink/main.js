43ca0410711acee09eddb0b40381476714fdc2c5
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-03-18 17:36:06 +0100

    just adding some example apps

---
+++ examples/iotapp-template-onboard-led-blink/main.js
@@ -0,0 +1,34 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+/*global */
+
+/*
+A simple node.js application intended to blink the onboard LED on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
+*/
+
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
+
+var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
+var ledState = true; //Boolean to hold the state of Led
+
+periodicActivity(); //call the periodicActivity function
+
+function periodicActivity()
+{
+  myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
+  ledState = !ledState; //invert the ledState
+  setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
+}
