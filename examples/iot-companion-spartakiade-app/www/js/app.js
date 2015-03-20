6c7a0f500b5c0f948012b4ead4e7863a06516104
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-03-20 18:26:04 +0100

    adding socket.io, this should do the trick

--- examples/iot-companion-spartakiade-app/www/js/app.js
+++ examples/iot-companion-spartakiade-app/www/js/app.js
@@ -1,4 +1,4 @@
-/*global angular:false, console:false */
+/*global angular:false, io:false, console:false */
 
 angular.module('app', ['ionic'])
 
@@ -20,19 +20,47 @@ angular.module('app', ['ionic'])
       $urlRouterProvider.otherwise('/connect');
     })
 
-    .controller('connectController', function ($scope, $state) {
+    // wrapper around socket.io so that it can be shared between controllers
+    .factory('socketIo', function () {
+    
+        var socket;
+            
+        return {        
+            connect: function(connetion) {
+                return (socket = io.connect('http://' + connetion.ip + ':' + connetion.port));
+            },
+            getSocket: function() {
+                return socket;
+            }
+        };
+    })
 
-        console.log($scope);
+    .controller('connectController', function ($scope, $state, socketIo) {
 
-        $scope.model = {
+        $scope.connection = {
             ip: "0.0.0.0",
             port: "1337"
         };
     
-        $scope.connect = function() {
+        $scope.connect = function() {  
             
-            $state.go('temperature');
+            var socket = socketIo.connect($scope.connection);
+            socket.on("connected", function (message) {
+                $state.go('temperature');
+            });
         };
+    })
+
+
+    .controller('temperatureController', function ($scope, socketIo) {
+
+        var socket = socketIo.getSocket();
+        socket.on("message", function (message) {
+            
+            $scope.$apply(function() {
+                $scope.temperature = message + " °C"; 
+            });
+
+        });
     
-        
     });
